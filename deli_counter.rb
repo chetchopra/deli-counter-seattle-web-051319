801d587a4403bfeab835db30f1812103818fad42
bustling-maker-4835 <chopra.chet@gmail.com>
2019-05-09 18:19:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,16 @@
 def line(arr)
   if arr.size() == 0 
-    puts "The lin eis currently empty"
+    puts "The line is currently empty"
   else
     count = 1
-    arr.each
+    str = "The line is currently: "
+    arr.each do |n|
+      str += "#{count}. #{n}"
+    end
+    puts str
+  end
+end
+      
+    
+    
+    
